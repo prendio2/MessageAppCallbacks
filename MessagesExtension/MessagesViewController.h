c35bcabee5d8b968355640fbfd2f90e1fab5dba7
Oisin Prendiville <oisin@supertop.co>
2016-09-28 14:21:39 +0200

    Initial Commit

---
+++ MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessagesExtension
+//
+//  Created by Oisin Prendiville on 28/09/2016.
+//  Copyright © 2016 Oisin Prendiville. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
